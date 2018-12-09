d6bf6bda4fcd093485c8787ed2b681ed85955b7c
Patrick Sacchet <pjsacchet@loyola.edu>
2018-12-09 15:35:33 -0500

    Final Update: Version 1.0

--- pacman.js
+++ pacman.js
@@ -1,6 +1,7 @@
 /**
 * Patrick Sacchet
-* Wed Nov 28, 2018 - Ver. 1.0
+* Wed Dec 9, 2018 - Ver. 1.0
+* The goal of this project is to recreate a simple representaiton of the classic pacman game
 *
 * Sources:
 * https://threejs.org/docs/#api/en/core/Object3D
@@ -86,15 +87,11 @@ audioLoader.load('/threejs/pac-man-intro.mp3', function(buffer) {
     sound.play();
 });
 
-
-
 // Creating keyboard variable which will be used for input
 var keyboard = new THREEx.KeyboardState();
 
 //Something to keep track of Score
 var score = 0;
-
-var pacDead;
 
 //Something to keep the user from smashing input
 var dt = 0;
@@ -108,14 +105,6 @@ makeBarriers(10, 1);
 
 //Adding pills to map
 addPills(1);
-
-// gui
-//var gui = new dat.GUI();
-//gui.width = 400;
-//h = gui.addFolder("Score");
-//h.open();
-//h.add(score, "score").name("Score");
-
 
 //Adding ghosts to the map
 var ghost1geometry = new THREE.PlaneGeometry(2.5, 5);
@@ -155,34 +144,36 @@ ghost4.width = 2.5;
 ghost4.height = 5;
 
 //Pause user interaction while song plays
-var played = false;
-
+//var played = false;
 
 // create animation clock and start animation
 var clock = new THREE.Clock();
 animate();
 
 // -----------------------------------------------------------------------------
-function animate() {
-
+//Animate function has many duties, uodates position of pacman according to user input,
+//checks for barrier/pill collision, checks for collision of ghosts, moves ghosts, and checks
+//to see if pacman has been defeated due to his proximity of a ghost
+function animate()
+{
     //Put this function in queue for another frame after this one
     requestAnimationFrame(animate);
-
-    //Get delta time: seconds passed since last frame, add overtime until time has reahced 0.09 seconds
+    //Get time since last frame
     var t = clock.getDelta();
+    //This variable used for pacman movement
     dt = dt + t;
+    //This variable used for ghost movement
     dg = dg + t;
-
     //This large if statement prevents the user from hitting the keys too fast, causing any clipping errors
     // and allowing the "waka waka" sound effect to not overlap
     if(dt > 0.25)
     {
-
     //Checking for keyboard input, if user presses w we move pacman up one
     if(keyboard.pressed("W"))
     {
       pacman.position.y = pacman.position.y + 1;
       camera.position = pacman.position;
+      //Play waka sound
       var listener = new THREE.AudioListener();
       camera.add(listener);
       var sound = new THREE.Audio(listener);
@@ -193,6 +184,7 @@ function animate() {
           sound.setVolume(0.5);
           sound.play();
       });
+      //Reset timer
       dt = 0;
       //Traversing the scene, analyzing the relevant object and checking to see if it collides
       scene.traverse( function( node )
@@ -248,7 +240,7 @@ function animate() {
     {
       pacman.position.y = pacman.position.y - 1;
       camera.position = pacman.position;
-      dt = 0;
+      //Play waka sound
       var listener = new THREE.AudioListener();
       camera.add(listener);
       var sound = new THREE.Audio(listener);
@@ -259,6 +251,8 @@ function animate() {
           sound.setVolume(0.5);
           sound.play();
       });
+      //Reset timer
+      dt = 0;
       //Traversing entire scene, only picking relevant objects
       scene.traverse( function( node )
       {
@@ -313,7 +307,7 @@ function animate() {
     {
       pacman.position.x = pacman.position.x + 1;
       camera.position = pacman.position;
-      dt = 0;
+      //Playing waka sound
       var listener = new THREE.AudioListener();
       camera.add(listener);
       var sound = new THREE.Audio(listener);
@@ -324,6 +318,8 @@ function animate() {
           sound.setVolume(0.5);
           sound.play();
       });
+      //Reset timer
+      dt = 0;
       //Traversing entire scene and only picking relevant objects
       scene.traverse( function( node )
       {
@@ -378,7 +374,7 @@ function animate() {
     {
       pacman.position.x = pacman.position.x - 1;
       camera.position = pacman.position;
-      dt = 0;
+      //Play waka sound
       var listener = new THREE.AudioListener();
       camera.add(listener);
       var sound = new THREE.Audio(listener);
@@ -389,6 +385,8 @@ function animate() {
           sound.setVolume(0.5);
           sound.play();
       });
+      //Reset timer
+      dt = 0;
       //Traversing entire scene, selecting appropiate barriers
       scene.traverse( function( node )
       {
@@ -437,35 +435,41 @@ function animate() {
         }
       } );
     }
-}
+  }
+    //Waiting half a second before moving ghosts again
     if(dg > 0.5)
     {
       moveGhosts(ghost1, ghost2, ghost3, ghost4);
+      //Reset the timer
       dg = 0;
     }
-
+    //Checking to see if pacman has collided with any of the ghosts, if any of them return true,
+    //pacman is remooved and if the user has more lives he is returned to the center of the map,
+    //ofhterwise the user is prompted with a game over screen
     if((isPacmanDead(ghost1, pacman)) || (isPacmanDead(ghost2, pacman)) || (isPacmanDead(ghost3, pacman))
     || (isPacmanDead(ghost4, pacman)))
     {
-      //var listener = new THREE.AudioListener();
-      //camera.add(listener);
-      //var sound = new THREE.Audio(listener);
-      //var audioLoader = new THREE.AudioLoader();
+      //Play death sound
       audioLoader.load('/threejs/pacman_death.wav', function(buffer) {
           sound.setBuffer( buffer );
           sound.setLoop(false);
           sound.setVolume(0.5);
           sound.play();
       });
+      //Remove pacman from the scene
       scene.remove(pacman);
+      //Add the waiting effect
       wait(3000);
+      //Check to see if the user has more lives
       if (lives > 0)
       {
+        //If so add pacman back at the start of the map
         scene.add(pacman);
         pacman.position.y = 1;
         pacman.position.x = -12;
         lives = lives -1;
       }
+      //Otherwise place a alert in the center of the screen and display the user's score
       else
       {
         alert("Sorry! You lose");
@@ -479,13 +483,15 @@ function animate() {
     //Render
     renderer.render(scene, camera);
 
-
 }
 // -----------------------------------------------------------------------------
+//This function creates barriers of specified width and height and spreads them throughout the map
 function makeBarriers(width, height)
 {
+  //Create initial barrier structure and color
   var rectgeometry = new THREE.PlaneGeometry(width, height);
   var rectmaterial = new THREE.MeshBasicMaterial({color: 0x1919A6, wireframe: true});
+  //Setting bounds of map
   var maxx = 162;
   var maxy = 72;
   for(var x = -maxx; x <= maxx; x += 20)
@@ -517,11 +523,13 @@ function makeBarriers(width, height)
 }
 
 // -----------------------------------------------------------------------------
+//Simple funciton to go through map, add pills of specified radius throughout the map
 function addPills(radius)
 {
+  //Create initial pill structure and color
   var pillgeo = new THREE.CircleGeometry(radius);
   var pillmat = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-
+  //Min and max values according to size of map
   minx = -151;
   miny = -61;
   maxx = 148;
@@ -532,6 +540,7 @@ function addPills(radius)
     scene.add(pill);
     pill.position.x = (x);
     pill.position.y = (y);
+    //This height is what allows the scene.traverse function to recognize it as a pill object
     pill.height = 0;
     for(var y = miny; y <= maxy; y += 20)
     {
@@ -544,15 +553,22 @@ function addPills(radius)
   }
 }
 // -----------------------------------------------------------------------------
+//This function generates random ints between -1 and 1, adds them to each ghosts current coordinates,
+//and updates their position accordingly
 function moveGhosts(ghost1, ghost2, ghost3, ground4)
 {
-
+  //Generating a random x and y value for the first ghost
   var randxgh1 = Math.floor((Math.random() * (2-(-1)) -1));
   var randygh1 = Math.floor((Math.random() * (2-(-1)) -1));
+  //Update x position, check for collision immediately after update
   ghost1.position.x = ghost1.position.x + randxgh1 ;
   checkGhostCollision(ghost1, randxgh1, randygh1);
+  //Update y position, check for collision immediately after update
   ghost1.position.y = ghost1.position.y + randygh1;
   checkGhostCollision(ghost1, randxgh1, randygh2);
+  //////////////////////////////////////////////////////////////////////////////
+  //This same sequence follows repeatedly for the remaining three ghosts
+  //////////////////////////////////////////////////////////////////////////////
   var randxgh2 = Math.floor((Math.random() * (2-(-1)) -1));
   var randygh2 = Math.floor((Math.random() * (2-(-1)) -1));
   ghost2.position.x = ghost2.position.x + randxgh2;
@@ -574,19 +590,23 @@ function moveGhosts(ghost1, ghost2, ghost3, ground4)
 }
 
 // -----------------------------------------------------------------------------
+//Checks collisions of ghosts around map, keeps them within the mao and prevents barrier collision
+//The ghost is passed in, as well as the changes in the ghost's x and y position and checks for
+//collisions accordingly
 function checkGhostCollision(ghost, xchange, ychange)
 {
+  //If the change in x position is positive (right)...
   if (xchange > 0)
   {
     scene.traverse( function( node )
     {
-      //Finding the max and min y values to ensure pacman is in front of the barrier
+      //Finding the max and min y values to ensure ghost is in front of the barrier
       var maxdown = (node.position.y - (node.height/2));
       var maxup = (node.height/2) + node.position.y;
       //Checking to see if it is a barrier inside map
       if (node instanceof THREE.Mesh && node.height < 150 && node.width < 150)
       {
-        //Checking for collision and to see if pacman is in front of the barrier
+        //Checking for collision and to see if ghost is in front of the barrier
         if(((ghost.position.x + 3 ) - node.position.x == 0) && (ghost.position.y >= maxdown && ghost.position.y <= maxup ))
         {
           //Placing ghost back into place
@@ -599,29 +619,29 @@ function checkGhostCollision(ghost, xchange, ychange)
         //Checking for collision
         if((ghost.position.x + 3)  - node.position.x  == 0)
         {
-          //Placing pacman back into place
+          //Placing ghost back into place
           ghost.position.x = ghost.position.x - 1;
         }
       }
     } );
   }
 
+  //If ghost is moving in negative x direction (left)...
   if (xchange < 0)
   {
     scene.traverse( function( node )
     {
-      //Finding the max and min y values to ensure pacman is in front of the barrier
+      //Finding the max and min y values to ensure ghost is in front of the barrier
       var maxdown = (node.position.y - (node.height/2));
       var maxup = (node.height/2) + node.position.y;
       //Checking to see if it is a barrier inside map
       if (node instanceof THREE.Mesh && node.height < 150 && node.width < 150)
       {
-        //Checking for collision and to see if pacman is in front of the barrier
+        //Checking for collision and to see if ghost is in front of the barrier
         if(((ghost.position.x - 3 ) - node.position.x == 0) && (ghost.position.y >= maxdown && ghost.position.y <= maxup ))
         {
           //Placing ghost back into place
           ghost.position.x = ghost.position.x + 1 ;
-          //renderer.render(scene, camera);
         }
       }
       //Checking to see if it is an outer boundary
@@ -630,30 +650,29 @@ function checkGhostCollision(ghost, xchange, ychange)
         //Checking for collision
         if((ghost.position.x - 3)  - node.position.x  == 0)
         {
-          //Placing pacman back into place
+          //Placing ghost back into place
           ghost.position.x = ghost.position.x + 1;
-          //renderer.render(scene, camera);
         }
       }
     } );
   }
 
+  //Checking to see if ghost is moving in positive y direction (up)...
   if (ychange > 0)
   {
     scene.traverse( function( node )
     {
-      //Setting x min and x max values to ensure pacman is infront of the barrier
+      //Setting x min and x max values to ensure ghost is infront of the barrier
       var maxleft = (node.position.x - (node.width/2));
       var maxright = (node.width/2) + node.position.x;
       //This excludes the outer boundaries of the map
       if (node instanceof THREE.Mesh && node.height < 150 && node.width < 150)
       {
-        //If pacman is colliding with the barrier, and is in front of the barrier
+        //If ghost is colliding with the barrier, and is in front of the barrier
         if(((ghost.position.y + 2)  - node.position.y  == 0) && (ghost.position.x >= maxleft && ghost.position.x <= maxright ))
         {
-          //Move pacman back to original spot
+          //Move ghost back to original spot
           ghost.position.y = ghost.position.y - 1;
-          //renderer.render(scene, camera);
         }
       }
       //If the barrier is large, then it is an outer barrier
@@ -662,30 +681,29 @@ function checkGhostCollision(ghost, xchange, ychange)
         //Checking for the collision
         if((ghost.position.y + 2)  - node.position.y  == 0)
         {
-          //Move pacman back into place
+          //Move ghost back into place
           ghost.position.y = ghost.position.y - 1;
-          //renderer.render(scene, camera);
         }
       }
     });
   }
 
+  //If ghost is moving in negative y direction (down)...
   if(ychange < 0)
   {
     scene.traverse( function( node )
     {
-      //Setting x min and x max values to ensure pacman is infront of the barrier
+      //Setting x min and x max values to ensure ghost is infront of the barrier
       var maxleft = (node.position.x - (node.width/2));
       var maxright = (node.width/2) + node.position.x;
       //This excludes the outer boundaries of the map
       if (node instanceof THREE.Mesh && node.height < 150 && node.width < 150 )
       {
-        //If pacman is colliding with the barrier, and is in front of the barrier
+        //If ghost is colliding with the barrier, and is in front of the barrier
         if(((ghost.position.y - 2)  - node.position.y  == 0) && (ghost.position.x >= maxleft && ghost.position.x <= maxright ))
         {
-          //Move pacman back to original spot
+          //Move ghost back to original spot
           ghost.position.y = ghost.position.y + 1;
-          //renderer.render(scene, camera);
         }
       }
       //If the barrier is large, then it is an outer barrier
@@ -694,23 +712,24 @@ function checkGhostCollision(ghost, xchange, ychange)
         //Checking for the collision
         if((ghost.position.y - 2)  - node.position.y  == 0)
         {
-          //Move pacman back into place
+          //Move ghost back into place
           ghost.position.y = ghost.position.y + 1;
-          //renderer.render(scene, camera);
         }
       }
     });
   }
 }
 // -----------------------------------------------------------------------------
+//This function checks the area around the ghost passed into as a parameter and the position of pacman,
+//if they are within the same perimeter this function returns true, otherwise returns false
 function isPacmanDead(ghost, pacman)
 {
+  //Checking a perimeter of three around pacman and the ghost
   for(var i = 0; i < 4; i ++)
   {
     if((((pacman.position.x + i) - (ghost.position.x +2) == 0) || ((pacman.position.x - i) - (ghost.position.x - 2) == 0))
     && (((pacman.position.y + i) - (ghost.position.y + 2) == 0) || ((pacman.position.y - i) - (ghost.position.y - 2) == 0)))
     {
-
       return true;
     }
     else
@@ -720,10 +739,13 @@ function isPacmanDead(ghost, pacman)
   }
 }
 // -----------------------------------------------------------------------------
-function wait(ms){
+//Wait function to add the waiting effect pacman provides on start and on death
+function wait(ms)
+{
    var start = new Date().getTime();
    var end = start;
-   while(end < start + ms) {
+   while(end < start + ms)
+   {
      end = new Date().getTime();
-  }
+   }
 }
